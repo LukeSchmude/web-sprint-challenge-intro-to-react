cf33fb0d7921b571a82770837c5b8ce7a06d6482
LukeSchmude <schmude2002@gmail.com>
2021-10-08 17:15:43 -0500

    information showing characters names and buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character';
 
-const App = () => {
+const App = (props) => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [who, setWho] = useState([])
+  const [currentCharacterId, setCurrentCharacterId] = useState(null)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const openDetails = id => {
+    setCurrentCharacterId(id)
+  }
+
+  const closedDetails = () => {
+    setCurrentCharacterId(null)
+  }
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(res => {
+      setWho(res.data)
+    }).catch(err => {
+      console.log(err);
+    })
+  },[])
+
+  const Who = props => (
+    <div className="charcter">
+      {props.info.name}
+      <button onClick={() => openDetails(props.info.id)}>
+        see more
+      </button>
+    </div>
+  )
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
+      {
+who.map(wh => {
+  return <Who key={wh.id} info={wh} />
+})
+}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
-// Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL } from './constants/constantsindex';
+
+
+
+
+
+export default function Character(props) {
+    const { character, close } = props;
+    const [details, setDetails] = useState([])
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}`)
+        .then(res => {
+            setDetails(res.data);
+        }).catch(err => {
+            console.log(err);
+        })
+    }, [character])
+
+    return (
+        <div className='character-container'>
+            <h2 className='character-name'>{details.name}</h2>
+        </div>
+    )
+}
